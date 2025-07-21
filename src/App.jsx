2162c1e1934597d827d95309a53f85379a63b61a
Shayan Abdullah <tuhinafroza07@gmail.com>
2025-07-22 00:46:55 +0600

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Images from './components/Images'
+import List from './components/List'
+import Button from './components/Button'
+const App = () => {
+
+
+  return (
+    <div>
+      <Button text="Download CV"/>
+      <List/>
+      <Images />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#00413D',
+        secondary: '#E5745D'
+      },
+       fontFamily: {
+      'jost': ["Jost"]
+    
+    }
+    },
+  },
+  plugins: [],
+}
